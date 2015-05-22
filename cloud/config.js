eaafc6fb2e16433ff125e733a575563f7198efcd
meowoodie <woodielove@163.com>
2015-05-22 19:36:24 +0800

    add new init params config

--- cloud/config.js
+++ cloud/config.js
@@ -26,14 +26,83 @@ exports.InitParams = {
                 nMix: 4,
                 covarianceType: "full"
             }
+        },
+        "fitness.running": {
+            nComponent: 4,
+            hmmParams: {
+                startProb: [0.4, 0.3, 0.1, 0.2],
+                transMat: [
+                    [0.2, 0.1, 0.3, 0.4],
+                    [0.3, 0.2, 0.2, 0.3],
+                    [0.1, 0.1, 0.1, 0.7],
+                    [0.1, 0.3, 0.4, 0.2]
+                ],
+                startProbPrior: [0.4, 0.3, 0.1, 0.2],
+                transMatPrior: [
+                    [0.2, 0.1, 0.3, 0.4],
+                    [0.3, 0.2, 0.2, 0.3],
+                    [0.1, 0.1, 0.1, 0.7],
+                    [0.1, 0.3, 0.4, 0.2]
+                ]
+            },
+            gmmParams: {
+                nMix: 4,
+                covarianceType: "full"
+            }
+        },
+        "work.office": {
+            nComponent: 4,
+            hmmParams: {
+                startProb: [0.4, 0.3, 0.1, 0.2],
+                transMat: [
+                    [0.2, 0.1, 0.3, 0.4],
+                    [0.3, 0.2, 0.2, 0.3],
+                    [0.1, 0.1, 0.1, 0.7],
+                    [0.1, 0.3, 0.4, 0.2]
+                ],
+                startProbPrior: [0.4, 0.3, 0.1, 0.2],
+                transMatPrior: [
+                    [0.2, 0.1, 0.3, 0.4],
+                    [0.3, 0.2, 0.2, 0.3],
+                    [0.1, 0.1, 0.1, 0.7],
+                    [0.1, 0.3, 0.4, 0.2]
+                ]
+            },
+            gmmParams: {
+                nMix: 4,
+                covarianceType: "full"
+            }
+        },
+        "shopping.mall": {
+            nComponent: 4,
+            hmmParams: {
+                startProb: [0.4, 0.3, 0.1, 0.2],
+                transMat: [
+                    [0.2, 0.1, 0.3, 0.4],
+                    [0.3, 0.2, 0.2, 0.3],
+                    [0.1, 0.1, 0.1, 0.7],
+                    [0.1, 0.3, 0.4, 0.2]
+                ],
+                startProbPrior: [0.4, 0.3, 0.1, 0.2],
+                transMatPrior: [
+                    [0.2, 0.1, 0.3, 0.4],
+                    [0.3, 0.2, 0.2, 0.3],
+                    [0.1, 0.1, 0.1, 0.7],
+                    [0.1, 0.3, 0.4, 0.2]
+                ]
+            },
+            gmmParams: {
+                nMix: 4,
+                covarianceType: "full"
+            }
         }
     }
 };
 
 exports.Algo = {
     GMMHMM: {
-        //train: "http://120.27.30.239:9047/trainingGMMHMM/",
-        train: "http://127.0.0.1:9010/trainingGMMHMM/",
+        train: "http://120.27.30.239:9047/trainingGMMHMM/",
+        //train: "http://127.0.0.1:9010/trainingGMMHMM/",
         trainRandomly: "http://120.27.30.239:9047/trainingGMMHMMrandomly/",
         classify: "http://120.27.30.239:9047/classifyGMMHMM/",
         getModel: dao.getRecentGMMHMM,
