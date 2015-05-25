aea458ede50b11e1ea6a56dee27e846bfbb55b25
meowoodie <woodielove@163.com>
2015-05-25 22:23:33 +0800

    add all init interface

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,8 @@
 var dao = require("cloud/dao/dao_model.js");
 var method = require("cloud/method.js");
 var algo = require("cloud/algo/model.js");
+var config = require("cloud/config.js");
+var _ = require("underscore");
 
 AV.Cloud.define("updateGMMHMM", function (request, response) {
     var tag = request.params.tag,
@@ -154,108 +156,31 @@ AV.Cloud.define("initModelParams", function (request, response) {
 });
 
 
+AV.Cloud.define("initAllEventsModelParams", function (request, response) {
+    var algo_type = request.params.algoType,
+        tag       = request.params.tag;
+    var date = new Date();
 
-//var obs = [[{"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//    {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//    {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//    {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//    {"motion": "walking", "sound": "others", "location": "chinese_restaurant"},
-//    {"motion": "walking", "sound": "tableware", "location": "chinese_restaurant"},
-//    {"motion": "sitting", "sound": "laugh", "location": "chinese_restaurant"},
-//    {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//    {"motion": "sitting", "sound": "tableware", "location": "residence"},
-//    {"motion": "sitting", "sound": "others", "location": "glass_store"}],
-//    [{"motion": "walking", "sound": "laugh", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "glass_store"},
-//        {"motion": "sitting", "sound": "laugh", "location": "glass_store"},
-//        {"motion": "sitting", "sound": "others", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "glass_store"},
-//        {"motion": "walking", "sound": "laugh", "location": "glass_store"},
-//        {"motion": "walking", "sound": "keyboard", "location": "glass_store"},
-//        {"motion": "sitting", "sound": "others", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "others", "location": "glass_store"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"}],
-//    [{"motion": "walking", "sound": "laugh", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "glass_store"},
-//        {"motion": "sitting", "sound": "tableware", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "tableware", "location": "residence"},
-//        {"motion": "walking", "sound": "others", "location": "glass_store"},
-//        {"motion": "sitting", "sound": "laugh", "location": "glass_store"},
-//        {"motion": "sitting", "sound": "talking", "location": "glass_store"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "keyboard", "location": "residence"}],
-//    [{"motion": "walking", "sound": "talking", "location": "glass_store"},
-//        {"motion": "sitting", "sound": "talking", "location": "residence"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "others", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "walking", "sound": "laugh", "location": "residence"},
-//        {"motion": "walking", "sound": "tableware", "location": "chinese_restaurant"}],
-//    [{"motion": "walking", "sound": "tableware", "location": "glass_store"},
-//        {"motion": "walking", "sound": "laugh", "location": "glass_store"},
-//        {"motion": "walking", "sound": "others", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "laugh", "location": "glass_store"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "laugh", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "laugh", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "tableware", "location": "chinese_restaurant"},
-//        {"motion": "walking", "sound": "others", "location": "chinese_restaurant"}],
-//    [{"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "walking", "sound": "laugh", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "others", "location": "glass_store"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "walking", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "laugh", "location": "residence"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "others", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "tableware", "location": "chinese_restaurant"}],
-//    [{"motion": "sitting", "sound": "keyboard", "location": "glass_store"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "tableware", "location": "chinese_restaurant"},
-//        {"motion": "walking", "sound": "talking", "location": "glass_store"},
-//        {"motion": "walking", "sound": "laugh", "location": "chinese_restaurant"},
-//        {"motion": "walking", "sound": "laugh", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "laugh", "location": "chinese_restaurant"},
-//        {"motion": "walking", "sound": "talking", "location": "chinese_restaurant"}],
-//    [{"motion": "walking", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "others", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "laugh", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "walking", "sound": "others", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "walking", "sound": "others", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "keyboard", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "others", "location": "glass_store"}],
-//    [{"motion": "sitting", "sound": "tableware", "location": "glass_store"},
-//        {"motion": "walking", "sound": "others", "location": "residence"},
-//        {"motion": "sitting", "sound": "tableware", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "walking", "sound": "others", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "laugh", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "walking", "sound": "others", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "laugh", "location": "chinese_restaurant"}],
-//    [{"motion": "walking", "sound": "talking", "location": "glass_store"},
-//        {"motion": "sitting", "sound": "others", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "glass_store"},
-//        {"motion": "sitting", "sound": "others", "location": "chinese_restaurant"},
-//        {"motion": "walking", "sound": "laugh", "location": "chinese_restaurant"},
-//        {"motion": "walking", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "walking", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "sitting", "sound": "talking", "location": "chinese_restaurant"},
-//        {"motion": "walking", "sound": "laugh", "location": "glass_store"}]];
+    var promises = [];
+    var eventsInfo = config.InitParams[algo_type];
+    _.keys(eventsInfo).forEach(function (event_label){
+        promises.push(method.initModelParams(algo_type, tag, event_label));
+    });
+    AV.Promise.all(promises).then(
+        function (model_id_list){
+            response.success({
+                code: 0,
+                modelsIdList: model_id_list,
+                message: "All Models init successfully! at " + date
+            });
+        },
+        function (error){
+            response.error({
+                code: 1,
+                message: error
+            });
+        }
+    );
+});
 
 
-//m = method.trainWithRandomObs("GMMHMM", "random_generated_base_model", "dining.chineseRestaurant", 10, 100);
-//m = method.trainWithSpecificObs("GMMHMM", "random_generated_base_model", "dining.chineseRestaurant", obs, "test for UpdateModel");
