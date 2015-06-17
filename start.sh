c2ec2bd1e94533f6a54faa9054e4c832e42bb8ef
meowoodie <woodielove@163.com>
2015-06-18 00:22:02 +0800

    caocaocaogit add start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,7 @@ export LC_APP_KEY="3sy9w8uwlr35xl54lja3rawyf8xjrhofxtvcwzng3blg7q31"
 export LC_APP_MASTER_KEY="9rr2aj6yaiwr0lkxn6jfmnfwnmextlc7xdznsg2i4di1ao81"
 export LC_APP_PORT=3000
 export APP_ENV="prod"
+
+
+
 npm start
